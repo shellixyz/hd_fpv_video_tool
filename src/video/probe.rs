5652c670bd6d021d3b338c337fd0ee0e6107b59d
Michel Pastor <shellixyz@gmail.com>
2022-12-04 02:40:19 +0100

    video::probe: improve error handling

--- src/video/probe.rs
+++ src/video/probe.rs
@@ -10,15 +10,19 @@ use super::resolution::Resolution;
 
 
 #[derive(Debug, Error)]
-#[error("failed to probe video file {file_path}: {error}")]
-pub struct Error {
-    file_path: PathBuf,
-    error: String,
+pub enum Error {
+    #[error("failed opening video file {file_path}: {error}")]
+    FFMpeg {
+        file_path: PathBuf,
+        error: ffmpeg::Error,
+    },
+    #[error("cannot find video stream in file: {0}")]
+    CannotFindVideoStream(PathBuf),
 }
 
 impl Error {
-    pub fn new<P: AsRef<Path>>(file_path: P, error: &str) -> Self {
-        Self { file_path: file_path.as_ref().to_path_buf(), error: error.to_owned() }
+    pub fn ffmpeg<P: AsRef<Path>>(file_path: P, error: ffmpeg::Error) -> Self {
+        Self::FFMpeg { file_path: file_path.as_ref().to_path_buf(), error }
     }
 }
 
@@ -39,12 +43,12 @@ pub fn probe<P: AsRef<Path>>(video_file: P) -> std::result::Result<Result, Error
     ffmpeg::log::set_level(ffmpeg::log::Level::Quiet);
 
     let input = ffmpeg::format::input(&video_file)
-        .map_err(|_| Error::new(&video_file, "failed to open video file"))?;
+        .map_err(|error| Error::ffmpeg(&video_file, error))?;
 
     let has_audio = input.streams().best(ffmpeg::media::Type::Audio).is_some();
 
     let video_stream = input.streams().best(ffmpeg::media::Type::Video)
-        .ok_or_else(|| Error::new(&video_file, "cannot find video stream"))?;
+        .ok_or_else(|| Error::CannotFindVideoStream(video_file.as_ref().to_path_buf()))?;
 
     let video_stream_parameters = video_stream.parameters();
     let (width, height) = unsafe { ((*video_stream_parameters.as_ptr()).width, (*video_stream_parameters.as_ptr()).height) };
@@ -64,8 +68,7 @@ pub fn probe<P: AsRef<Path>>(video_file: P) -> std::result::Result<Result, Error
 
     let frame_rate = video_stream.rate();
 
-    let frame_count = u64::try_from(video_stream.frames())
-        .map_err(|_| Error::new(&video_file, "failed to get frame count"))?;
+    let frame_count = u64::try_from(video_stream.frames()).unwrap();
 
     Ok(Result { frame_count, frame_rate, has_audio, resolution, video_codec })
 }
